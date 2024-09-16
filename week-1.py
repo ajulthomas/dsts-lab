6a221b26b830f3aef75711a6ce25be90e06ff818
Ajul Thomas <ajul.thomas@outlook.com>
2024-09-16 21:08:04 +1000

    initial commit

---
+++ week-1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Week 1 - Introduction
+
+# ### Excercise - 1
+
+# In[5]:
+
+
+print("Hello World")
+print("what's your name ?")
+name = input()
+print("It's goog to meet you, " + name)
+print("Your name is of length " + str(len(name)))
+print("whats's your age ? ")
+age = input()
+print("You will be "+str(int(age)+1)+" years old in a year")
+
+
+# ### Excercise - 2 
+
+# In[ ]:
+
+
+
+
